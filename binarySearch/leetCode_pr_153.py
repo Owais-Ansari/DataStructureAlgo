e98f36f5d258744ab290113ef18032b35a7ffae6
Owaish <owais.uaarav9@gmail.com>
2023-05-30 23:04:46 +0530

    Update leetCode_pr_153.py

--- binarySearch/leetCode_pr_153.py
+++ binarySearch/leetCode_pr_153.py
@@ -1,3 +1,4 @@
+'''
 Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = '[0,1,2,4,5,6,7]' might become:
 
 `[4,5,6,7,0,1,2]` if it was rotated 4 times.
@@ -7,8 +8,9 @@ Notice that rotating an array `[a[0], a[1], a[2], ..., a[n-1]]` 1 time results i
 Given the sorted rotated array nums of unique elements, return the minimum element of this array.
 
 You must write an algorithm that runs in `O(log n)` time.
+'''
 
-***solution***
+#***solution***
 '''
 Input : Rotated sorter array of unique integers
 OutPut : Minimun number in the list
