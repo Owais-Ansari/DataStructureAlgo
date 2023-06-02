ef79bfae20d1790304ee0e929e23310dee11912a
Owaish <owais.uaarav9@gmail.com>
2023-06-02 23:06:37 +0530

    Easy

---
+++ arrays/leet_pr1.py
@@ -0,0 +1,24 @@
+'''Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.'''
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        for indx in range(len(nums)):
+            low = indx + 1
+            high = len(nums) - 1
+            while low <= high:
+                if nums[low] + nums[indx] == target:
+                    return [low, indx]
+                low += 1
+        return []
+        
